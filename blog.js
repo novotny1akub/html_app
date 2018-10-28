3ffb6aa016da13fb157094be749a8d0223561544
Jakub Novotný <39886440+sivookydemon@users.noreply.github.com>
2018-10-28 15:24:23 +0100

    upload postu a download z Firebase

--- blog.js
+++ blog.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Firebase connection config
 var config = {
 apiKey: "AIzaSyAXss0HuO6uUcBFaPelQiMPw3ljO-NlHIY",
 authDomain: "greenfoxblog.firebaseapp.com",
@@ -8,30 +8,77 @@ storageBucket: "greenfoxblog.appspot.com",
 messagingSenderId: "376688960466"
 };
 
-
 firebase.initializeApp(config);
 fb = firebase.database();
 
-// button click event
-$('button').on('click', (event) => {
 
+
+
+
+
+// LOADING existing posts
+fb.ref("posts").once('value').then(data => {
+
+  // getting data
+  let savedPosts = data.val();
+
+  // looping through data fetched from Firebase
+  var i;
+  for (i = 1; i < savedPosts.length; i++) { 
+    console.log(i)
+
+    $('div').append(
+      `
+      <h2> ${savedPosts[i].title} </h2>
+      <p> ${savedPosts[i].text} </p>
+      `
+    )
+
+  }
+
+  });
+
+
+// button click event updating the database
+$('button').on('click', (event) => {
+  // number of existing posts (working)
+  fb.ref("posts").once('value').then(data => {
+    let nr_posts
+    if(data.val()){
+      nr_posts = data.val().length ; // data.val().length
+    } else {
+      nr_posts = 1
+    }
+    // updating the current posts to Firebase
+    let path = "posts/" + String( nr_posts );
+    let dataToSave = {
+    title: $('#title').val(),
+    text: $('#copied_text').val()
+    };
+    fb.ref(path).set(dataToSave);
+
+    $('div').append(
+      `
+      <h2> ${dataToSave.title} </h2>
+      <p> ${dataToSave.text} </p>
+      `
+    )
+
+    })
+
+  /*
   // getting data ready saving post
-  let path = "posts/1";
+  let path = "posts/" + "2";
   let dataToSave = {
   title: $('#title').val(),
   text: $('#copied_text').val()
   };
 
   fb.ref(path).set(dataToSave);
-  // clear the text area
+  */
 
   })
 
-// loading existing posts
-fb.ref("posts").once('value').then(data => {
-let savedPosts = data.val();
-console.log(savedPosts)
-});
 /*
 // UPDATING
 let samePath = "posts/1";
